959c54b9a5a6b423633e6049c00c21eeb387624e
gerg <gregross.dev@gmail.com>
2020-12-19 09:49:26 -0500

    add node

---
+++ node/node-express--server/server.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+
+const app = express(); 
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+app.use(express.static(__dirname + '/public'))
+
+
+app.listen(3000); 
